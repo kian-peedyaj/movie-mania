33227d89849109dc0f09fd26c2cab3d8bd793b41
Jaydeep Ajitbhai Naik <jnaik@nuskin.com>
2024-06-23 02:55:10 +0530

    first step

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,39 @@
-import Image from "next/image";
+// "use client"
+
+import * as React from "react"
+import Link from "next/link"
+
+// import { cn } from "@/lib/utils"
+// import { Icons } from "@/components/icons"
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu"
+import { Dashboard } from "./components/dashboard"
 
 export default function Home() {
-  return (
-    <main>
-      <h1>This is HOME</h1>
-    </main>
-  );
+  return <main>
+    <Dashboard />
+    {/* <div className="flex items-center p-4 gap-4">
+      <h5 className="font-extrabold text-xl">
+        Movie Mania
+      </h5>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <Link href="/" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Home
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div> */}
+  </main>
 }
